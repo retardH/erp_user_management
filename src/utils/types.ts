3298c5fb8c2cca41e1952b7f99a7c7482963697e
HtetZarni <loowmaad@gmail.com>
2024-03-05 19:51:06 +0630

    adjust styling

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -15,3 +15,90 @@ export type Role = {
   id: number;
   name: string;
 };
+
+// Define interfaces for Supabse Database Tables
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[];
+
+export type Database = {
+  public: {
+    Tables: {
+      roles: {
+        Row: {
+          createdAt: string;
+          id: number;
+          name: string | null;
+        };
+        Insert: {
+          createdAt?: string;
+          id?: number;
+          name?: string | null;
+        };
+        Update: {
+          createdAt?: string;
+          id?: number;
+          name?: string | null;
+        };
+        Relationships: [];
+      };
+      users: {
+        Row: {
+          createdAt: string;
+          email: string | null;
+          firstName: string | null;
+          id: number;
+          isActive: boolean | null;
+          lastName: string | null;
+          roleId: number | null;
+          username: string | null;
+        };
+        Insert: {
+          createdAt?: string;
+          email?: string | null;
+          firstName?: string | null;
+          id?: number;
+          isActive?: boolean | null;
+          lastName?: string | null;
+          roleId?: number | null;
+          username?: string | null;
+        };
+        Update: {
+          createdAt?: string;
+          email?: string | null;
+          firstName?: string | null;
+          id?: number;
+          isActive?: boolean | null;
+          lastName?: string | null;
+          roleId?: number | null;
+          username?: string | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'public_users_roleId_fkey';
+            columns: ['roleId'];
+            isOneToOne: false;
+            referencedRelation: 'roles';
+            referencedColumns: ['id'];
+          },
+        ];
+      };
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      [_ in never]: never;
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
+};
